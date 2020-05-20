46baa0bb63e1987b2cd1921c9fea6df25d6791e7
malyz <mattuy001@gmail.com>
2020-05-20 21:38:59 +1200

    added pokemon get by id

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -6,4 +6,11 @@ router.get('/pokemon', async (req, res) => {
   res.json(data)
 })
 
+router.get('/pokemon/:id', async (req, res) => {
+  const { data } = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+  )
+  res.json(data)
+})
+
 module.exports = router
