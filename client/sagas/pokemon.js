01e2c453632e21b8061d81e3c3444a7f308fd799
malyz <mattuy001@gmail.com>
2020-05-21 22:10:43 +1200

    added saga for request api pokemon

--- client/sagas/pokemon.js
+++ client/sagas/pokemon.js
@@ -2,7 +2,7 @@ import api from '../api'
 import { takeEvery, call, put } from 'redux-saga/effects'
 
 import { receiveApiAllPokemon } from '../store/actions/pokemon'
-import { REQUEST_API_ALL_POKEMON } from '../store/types'
+import { REQUEST_API_ALL_POKEMON, REQUEST_API_POKEMON } from '../store/types'
 
 function* callRequestAllPokemon(action) {
   const { data } = yield call(api.get, '/pokemon')
@@ -11,4 +11,13 @@ function* callRequestAllPokemon(action) {
 
 export function* requestAllPokemonSaga() {
   yield takeEvery(REQUEST_API_ALL_POKEMON, callRequestAllPokemon)
+}
+
+function* callRequestPokemon(action) {
+  const { data } = yield call(api.get, `/pokemon/${action.payload}`)
+  yield put(receiveApiAllPokemon(data))
+}
+
+export function* requestPokemonSaga() {
+  yield takeEvery(REQUEST_API_POKEMON, callRequestPokemon)
 }
