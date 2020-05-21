9dbd166b2bb8e8df2e2244b99ad31cc5febd91fb
malyz <mattuy001@gmail.com>
2020-05-21 22:07:19 +1200

    added reducer for request 1 pokemon

--- client/store/reducers/pokemon.js
+++ client/store/reducers/pokemon.js
@@ -1,4 +1,4 @@
-import { RECEIVE_API_ALL_POKEMON } from '../types'
+import { RECEIVE_API_ALL_POKEMON, RECEIVE_API_POKEMON } from '../types'
 
 const INITIAL_STATE = {
   all: [],
@@ -9,6 +9,8 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case RECEIVE_API_ALL_POKEMON:
       return { ...state, all: action.payload }
+    case RECEIVE_API_POKEMON:
+      return { ...state, selected: action.payload }
     default:
       return state
   }
