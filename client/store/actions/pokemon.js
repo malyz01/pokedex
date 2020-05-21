6798372603212dd79e19fe78c76530aeb1789bce
malyz <mattuy001@gmail.com>
2020-05-21 22:06:15 +1200

    added action creator for request 1 pokemon

--- client/store/actions/pokemon.js
+++ client/store/actions/pokemon.js
@@ -1,4 +1,9 @@
-import { REQUEST_API_ALL_POKEMON, RECEIVE_API_ALL_POKEMON } from '../types'
+import {
+  REQUEST_API_ALL_POKEMON,
+  RECEIVE_API_ALL_POKEMON,
+  REQUEST_API_POKEMON,
+  RECEIVE_API_POKEMON
+} from '../types'
 
 export const requestApiAllPokemon = () => ({
   type: REQUEST_API_ALL_POKEMON
@@ -6,5 +11,14 @@ export const requestApiAllPokemon = () => ({
 
 export const receiveApiAllPokemon = (pokemon) => ({
   type: RECEIVE_API_ALL_POKEMON,
+  payload: pokemon
+})
+
+export const requestApiPokemon = () => ({
+  type: REQUEST_API_POKEMON
+})
+
+export const receiveApiPokemon = (pokemon) => ({
+  type: RECEIVE_API_POKEMON,
   payload: pokemon
 })
