3007ee664b4eb2ab3c3bb0e32b1aa93623fd50c6
malyz <mattuy001@gmail.com>
2020-05-21 16:44:55 +1200

    uninstalled thunk, added config for redux-saga

--- client/index.js
+++ client/index.js
@@ -2,13 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import createSageMiddleware from 'redux-saga'
 
 import App from './components/App'
 import reducers from './store/reducers'
 
+const sagaMiddleware = createSageMiddleware()
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+)
+// sagaMiddleware.run()
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
