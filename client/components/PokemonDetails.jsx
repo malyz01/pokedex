923f3ff31f2cc3d35994042f5e0baad38317ab25
malyz <mattuy001@gmail.com>
2020-05-23 09:08:37 +1200

    added tab

--- client/components/PokemonDetails.jsx
+++ client/components/PokemonDetails.jsx
@@ -7,11 +7,20 @@ const PokemonDetails = ({ pokemon }) => {
       <div className="card-image">
         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       </div>
-      <div className="card-content">
-        <div>Abilities</div>
-        {abilities.map((a) => (
-          <div>{a}</div>
-        ))}
+      <div className="card-tabs">
+        <ul className="tabs tabs-fixed-width">
+          <li className="tab">
+            <a href="#abilities">Abilities</a>
+          </li>
+          <li className="tab">Info</li>
+        </ul>
+      </div>
+      <div class="card-content grey lighten-4">
+        <div id="abilities">
+          {abilities.map((a) => (
+            <div>{a}</div>
+          ))}
+        </div>
       </div>
     </>
   )
