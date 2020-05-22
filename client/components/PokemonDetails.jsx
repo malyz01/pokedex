597c26d633652bd4e911949401e4f5e9f373c339
malyz <mattuy001@gmail.com>
2020-05-22 22:36:24 +1200

    finished with pokemon details component

--- client/components/PokemonDetails.jsx
+++ client/components/PokemonDetails.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const PokemonDetails = ({ pokemon }) => {
-  return <div></div>
+  const abilities = pokemon.abilities.map((a) => a.ability.name)
+  return (
+    <>
+      <div className="card-image">
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      </div>
+      <div classname="card-content">
+        <div>Abilities</div>
+        {abilities.map((a) => (
+          <div>{a}</div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default PokemonDetails
