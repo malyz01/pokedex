ba189369248feae352530d13fb70bc83b4035e9e
malyz <mattuy001@gmail.com>
2020-05-22 20:20:37 +1200

    added redis set for pokemon

--- server/middleware/redis.js
+++ server/middleware/redis.js
@@ -1,4 +1,10 @@
 const redis = require('redis')
-const REDIS_PORT = process.env.REDIS_PORT || 6379
 
-module.exports = redis.createClient(REDIS_PORT)
+const REDIS_PORT = process.env.REDIS_PORT || 6379
+const client = redis.createClient(REDIS_PORT)
+
+module.cache = (req, res, next) => {
+  client.get('pokemon')
+}
+
+module.exports = client
