ed5f60ecc3b1f1780dd763020663b43dfab6e5c1
malyz <mattuy001@gmail.com>
2020-05-24 12:10:52 +1200

    added basic styles for pokemon details

--- client/components/PokemonDetails.jsx
+++ client/components/PokemonDetails.jsx
@@ -2,20 +2,30 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
+import './pokemonDetails.css'
 
 const Abilities = ({ pokemon }) => {
   const abilities = pokemon.abilities.map((a) => a.ability.name)
   return (
-    <div>
-      {abilities.map((a) => (
-        <div key={a}>{a}</div>
+    <div className="pokemon">
+      {abilities.map((a, i) => (
+        <div key={a}>{`${i + 1}. ${a}`}</div>
       ))}
     </div>
   )
 }
 
+const Info = ({ pokemon }) => {
+  return (
+    <div className="pokemon">
+      <div>Name: {pokemon.name}</div>
+      <div>Height: {pokemon.height}</div>
+      <div>Weight: {pokemon.weight}</div>
+    </div>
+  )
+}
+
 const PokemonDetails = ({ pokemon }) => {
-  const abilities = pokemon.abilities.map((a) => a.ability.name)
   return (
     <>
       <Card.Img
@@ -23,8 +33,18 @@ const PokemonDetails = ({ pokemon }) => {
         src={pokemon.sprites.front_default}
         alt={pokemon.name}
       />
-      <Abilities pokemon={pokemon} />
-      <Card.Text></Card.Text>
+      <Tabs defaultActiveKey="info">
+        <Tab className="pokemonDetailTabContent" eventKey="info" title="Info">
+          <Info pokemon={pokemon} />
+        </Tab>
+        <Tab
+          className="pokemonDetailTabContent"
+          eventKey="abilities"
+          title="Abilities"
+        >
+          <Abilities pokemon={pokemon} />
+        </Tab>
+      </Tabs>
     </>
   )
 }
