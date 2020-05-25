5cbf0aa579e5fa5e5d289be1a3f1513ce541b97d
malyz <mattuy001@gmail.com>
2020-05-25 21:53:30 +1200

    created dummy data

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,1 +1,20 @@
 const db = require('../connection')
+const { STRING } = require('sequelize')
+
+const User = db.define('user', {
+  firstName: {
+    type: STRING
+  },
+  lastName: {
+    type: STRING
+  }
+})
+
+User.create({
+  firstName: 'Fuwoo',
+  lastName: 'Uy'
+})
+
+User.findAll().then((users) => console.log(users[2].dataValues))
+
+module.exports = User
