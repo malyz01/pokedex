f168cdfada1e1fd83f7e451cf3349d2d68e5728e
malyz <mattuy001@gmail.com>
2020-06-17 19:27:24 +1200

    downgrade axios, refactor food api route

--- server/routes/food.js
+++ server/routes/food.js
@@ -1,21 +1,21 @@
 const router = require('express').Router()
 const axios = require('axios')
 
-const api = { params: { apiKey: process.env.FOOD_API } }
+const api = axios.create({
+  baseURL: process.env.FOOD_URL,
+  params: {
+    apiKey: process.env.FOOD_API
+  }
+})
 
-// /api/v1/food
-router.get('/', async (req, res) => {
-  const params = req.body || {}
+// /api/v1/food/random
+router.get('/random', async (req, res) => {
   try {
-    const result = await axios.get(
-      'https://api.spoonacular.com/recipes/random',
-      api
-    )
+    const result = await api.get('/random')
     res.json(result.data)
   } catch (err) {
     console.log(err)
-    // console.log(err.response.data)
-    // res.json(err.response.data.message)
+    res.json('Something went wrong in Get - food api')
   }
 })
 
