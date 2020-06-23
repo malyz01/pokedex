2aafbc54bda5673dc94c8c0ee873cecb93181b26
malyz <mattuy001@gmail.com>
2020-06-23 18:53:11 +1200

    added action creator for spoonacular

---
+++ client/store/actions/spoonacular.js
@@ -0,0 +1,14 @@
+import {
+  REQUEST_API_QUERY_SPOONACULAR,
+  RECEIVE_API_QUERT_SPOONACULAR
+} from '../types'
+
+export const requestApiSpoonacular = (payload) => ({
+  type: REQUEST_API_QUERY_SPOONACULAR,
+  payload
+})
+
+export const receiveApiSpoonacular = (payload) => ({
+  type: RECEIVE_API_QUERT_SPOONACULAR,
+  payload
+})
