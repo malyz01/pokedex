5340926ee9d9ba674370d838616de7285786414e
malyz <mattuy001@gmail.com>
2020-06-23 15:30:17 +1200

    added additional layout for edamam search

--- client/components/Edamam/index.jsx
+++ client/components/Edamam/index.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 const index = () => {
+  const [q, setQ] = useState('')
+  const [result, setResult] = useState('')
+
+  const handleChange = (e) => {
+    setQ(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    setResult(q)
+    setQ('')
+  }
+
   return (
     <div className="container mt-4">
-      <div className="jumbotron">Search field</div>
+      <div className="jumbotron">
+        <label className="mr-3">Query</label>
+        <input
+          className="mr-3"
+          name="q"
+          type="text"
+          value={q}
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit} className="btn btn-primary">
+          Submit
+        </button>
+      </div>
       <hr className="mb-5" />
-      <div className="EdamamSearchResult"></div>
+      <div className="EdamamSearchResult p-2">
+        <div>Result for: {result}</div>
+      </div>
     </div>
   )
 }
