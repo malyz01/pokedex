735179bef62a3ec0a637c60d90fd73f5cf78fe7d
malyz <mattuy001@gmail.com>
2020-06-23 15:45:35 +1200

    added redux saga edamam api call

---
+++ client/sagas/edamam.js
@@ -0,0 +1,24 @@
+import api from '../api'
+import { takeLatest, takeEvery, call, put } from 'redux-saga/effects'
+
+import { receiveApiEdamam } from '../store/actions/edamam'
+import { setError } from '../store/actions/error'
+import { REQUEST_API_QUERY_EDAMAM } from '../store/types'
+
+async function apiPostCall(body) {
+  return await api.post('/food/edamam/search', body)
+}
+
+function* callQueryEdamam(action) {
+  try {
+    const { data } = yield call(apiPostCall, action.payload)
+    yield put(receiveApiEdamam(data))
+  } catch (err) {
+    const { status, data } = err.response
+    yield put(setError(status, data))
+  }
+}
+
+export default function* requestPokemonSaga() {
+  yield takeLatest(REQUEST_API_QUERY_EDAMAM, callQueryEdamam)
+}
--- client/store/actions/edamam.js
+++ client/store/actions/edamam.js
@@ -1,10 +1,10 @@
 import { REQUEST_API_QUERY_EDAMAM, RECEIVE_API_QUERT_EDAMAM } from '../types'
 
-export const requestApiAllPokemon = () => ({
+export const requestApiEdamam = () => ({
   type: REQUEST_API_QUERY_EDAMAM
 })
 
-export const receiveApiAllPokemon = (edamam) => ({
+export const receiveApiEdamam = (edamam) => ({
   type: RECEIVE_API_QUERT_EDAMAM,
   payload: edamam
 })
