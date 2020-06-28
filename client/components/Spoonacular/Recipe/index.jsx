03bb10afe269198dcac35b2ec4b22f23c732b46e
malyz <mattuy001@gmail.com>
2020-06-28 18:02:17 +1200

    displayed recipe info

--- client/components/Spoonacular/Recipe/index.jsx
+++ client/components/Spoonacular/Recipe/index.jsx
@@ -5,10 +5,39 @@ import { requestApiRecipeInfo } from '../../../store/actions/spoonacular'
 
 const index = (props) => {
   const recipeId = props.match.params.id
+
   useEffect(() => {
     props.requestApiRecipeInfo(recipeId)
   }, [])
-  return <div>Recipe details</div>
+
+  const recipeDetails = Object.keys(props.info)
+  if (!recipeDetails.length) return <h1>No Recipe Info</h1>
+  return (
+    <div>
+      {recipeDetails.map((r, i) => {
+        return (
+          <div
+            key={i}
+            className="mb-5 mx-auto"
+            style={{ width: '80%', border: '2px solid black' }}
+          >
+            <div>{r}</div>
+            {Array.isArray(props.info[r]) ? (
+              props.info[r].map((x, ind) => (
+                <pre key={ind}>{JSON.stringify(x, null, 2)}</pre>
+              ))
+            ) : (
+              <pre>{JSON.stringify(props.info[r], null, 3)}</pre>
+            )}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
-export default connect(null, { requestApiRecipeInfo })(index)
+const mapStateToProps = (state) => ({
+  info: state.spoonacular.recipe
+})
+
+export default connect(mapStateToProps, { requestApiRecipeInfo })(index)
